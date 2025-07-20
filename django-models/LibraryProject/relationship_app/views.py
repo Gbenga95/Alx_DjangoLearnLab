4eb1c1e68a8538ac52370196076e097e4c39d83f
Gbenga <gbengaemmanuel450@gmail.com>
2025-07-20 17:12:52 +0100

    worked on login, logout and register

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.views import View
 
 from .models import Library, Book
 
@@ -22,3 +27,25 @@ class BookListView(ListView):
 
     def get_queryset(self):
         return Book.objects.all()
+    
+
+
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
