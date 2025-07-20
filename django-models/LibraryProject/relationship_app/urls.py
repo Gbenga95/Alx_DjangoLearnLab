c97a770400ccc59a7fb9f79acfec06a09db74fb4
Gbenga <gbengaemmanuel450@gmail.com>
2025-07-20 17:19:16 +0100

    worked on login, logout and register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 from .views import list_books, LibraryDetailView
 from .views import RegisterView, CustomLoginView, CustomLogoutView
 urlpatterns = [
@@ -7,5 +9,9 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
+
 
