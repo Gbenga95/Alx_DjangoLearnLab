7c8cb8a6751cc4dda45b8b3ba48e3c9b48216690
Gbenga <gbengaemmanuel450@gmail.com>
2025-07-20 16:58:42 +0100

    Updated the views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from django.views.generic.detail import DetailView
+
 from .models import Library, Book
 
 class LibraryDetailView(DetailView):
