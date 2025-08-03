371fddf31a8928a85a183d3ba926bb26ebb3aec7
Gbenga <gbengaemmanuel450@gmail.com>
2025-08-03 23:40:40 +0100

    Test authentication and permission

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,6 +5,9 @@ from rest_framework import viewsets
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Review
+from .serializers import ReviewSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -14,3 +17,24 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+from rest_framework.permissions import IsAdminUser, SAFE_METHODS, BasePermission
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
